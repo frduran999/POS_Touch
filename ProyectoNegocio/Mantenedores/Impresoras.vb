13b41980f0fd157f99093ebef11c65ac7c60c24c
frduran <fduran@disofi.cl>
2019-11-14 02:30:07 -0330

    Final 1

--- ProyectoNegocio/Mantenedores/Impresoras.vb
+++ ProyectoNegocio/Mantenedores/Impresoras.vb
@@ -1,10 +1,10 @@
 ﻿Imports ProyectoService
 
 Public Class Impresoras
-    Public Function GrabaImpresoras(ByVal primera As String, ByVal segunda As String, ByVal tercera As String, ByVal Id As Integer) As String
+    Public Function GrabaImpresoras(ByVal primera As String, ByVal Id As Integer) As String
         Dim resp As String = ""
         Dim bsp As New ImpresoraService
-        resp = bsp.GrabaImpresoras(primera, segunda, tercera, Id)
+        resp = bsp.GrabaImpresoras(primera, Id)
         Return resp
     End Function
     Public Function GetImpresoras() As DataTable
